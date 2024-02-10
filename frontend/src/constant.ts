73c4823e0fdac01c1b2281344e7bc8229df115de
Ruchir28 <ruchirnakra28@gmail.com>
2024-02-10 18:00:39 +0530

    Setting up Env Variables

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,2 +1,3 @@
-export const BACKEND_URL = "http://localhost:8000";
-export const WEBSOCKET_URL = "ws://localhost:8000/ws";
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8000";
+
+export const WEBSOCKET_URL = process.env.REACT_APP_WS_URL ? process.env.REACT_APP_WS_URL : "ws://localhost:8000/ws";
